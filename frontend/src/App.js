da47cc00344fcb9f48f0724255e4107b00fab6c9
aslesha <aslesha.29march@gmail.com>
2021-03-22 04:01:20 +0530

    Third Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Container } from "react-bootstrap";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import HomeScreen from "./screens/HomeScreen";
-import ProductScreen from "./screens/ProductScreen";
+import React from "react"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import HomeScreen from "./screens/HomeScreen"
+import ProductScreen from "./screens/ProductScreen"
+import CartScreen from "./screens/CartScreen"
+import LoginScreen from "./screens/LoginScreen"
+import RegisterScreen from "./screens/RegisterScreen"
+import ProfileScreen from "./screens/ProfileScreen"
+
 const App = () => {
 	//arrow function
 	return (
@@ -12,13 +17,17 @@ const App = () => {
 			<Header />
 			<main className="py-3">
 				<Container>
-					<Route path="/" component={HomeScreen} exact />
+					<Route path="/login" component={LoginScreen} />
+					<Route path="/register" component={RegisterScreen} />
+					<Route path="/profile" component={ProfileScreen} />
 					<Route path="/product/:id" component={ProductScreen} />
+					<Route path="/cart/:id?" component={CartScreen} />
+					<Route path="/" component={HomeScreen} exact />
 				</Container>
 			</main>
 			<Footer />
 		</Router>
-	);
-};
+	)
+}
 
-export default App;
+export default App
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,17 @@
-import React from "react";
-import { LinkContainer } from "react-router-bootstrap";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import React from "react"
+//useDispatch-call an action(login action)
+//useSelector-bring something in from redux state(user login state)
+import { useDispatch, useSelector } from "react-redux"
+import { LinkContainer } from "react-router-bootstrap"
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap"
+import { logout } from "../actions/userActions"
 const Header = () => {
+	const dispatch = useDispatch()
+	const userLogin = useSelector((state) => state.userLogin)
+	const { userInfo } = userLogin
+	const logoutHandler = () => {
+		dispatch(logout())
+	}
 	return (
 		<header>
 			<Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
@@ -20,17 +30,28 @@ const Header = () => {
 								</Nav.Link>
 								{/*fas=font-awesome*/}
 							</LinkContainer>
-							<LinkContainer to="/login">
-								<Nav.Link>
-									<i className="fas fa-user"></i>Sign In
-								</Nav.Link>
-							</LinkContainer>
+							{userInfo ? (
+								<NavDropdown title={userInfo.name} id="username">
+									<LinkContainer to="/profile">
+										<NavDropdown.Item>Profile</NavDropdown.Item>
+									</LinkContainer>
+									<NavDropdown.Item onClick={logoutHandler}>
+										Logout
+									</NavDropdown.Item>
+								</NavDropdown>
+							) : (
+								<LinkContainer to="/login">
+									<Nav.Link>
+										<i className="fas fa-user"></i>Sign In
+									</Nav.Link>
+								</LinkContainer>
+							)}
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
 			</Navbar>
 		</header>
-	);
-};
+	)
+}
 
-export default Header;
+export default Header
